58c63ba6699735ae782997fdf63f7e505064f4e7
Valexr <valexr@gmail.com>
2021-10-14 21:47:52 +0300

    remove undeed(now) active props

--- src/lib/components/Table/Table.svelte
+++ src/lib/components/Table/Table.svelte
@@ -14,7 +14,7 @@
 	</thead>
 	<tbody>
 		{#each rows as row}
-			<tr class:active={row.active}>
+			<tr class:active={row}>
 				{#each cols as col}
 					<td>{row[col]}</td>
 				{/each}
@@ -25,48 +25,11 @@
 
 <script lang="ts" context="module">
 	import type { Offset } from '../../types/position';
-
-	type Row = {
-		active?: boolean;
-	};
-
-	// interface ObjectConstructor {
-	// 	keys<T>(o: T): ObjectKeys<T>;
-	// }
-
-	// type ObjectKeys<T> = T extends object
-	// 	? (keyof T)[]
-	// 	: T extends number
-	// 	? []
-	// 	: T extends Array<any> | string
-	// 	? string[]
-	// 	: never;
-
-	// interface ObjectConstructor {
-	// 	//...
-	// 	keys(o: object): string[];
-	// 	keys(o: {}): string[];
-	// }
-	// interface UserUpdate {
-	// 	name: string;
-	// }
-
-	// const updatableFields = getWhitelistedKeys<Rows>({
-	// 	active: false,
-	// });
-
-	// function getWhitelistedKeys<T>(config: { [key in keyof T]: boolean }) {
-	// 	return Object.entries(config)
-	// 		.filter(([_, value]) => !!value)
-	// 		.map(([key]) => key as keyof T);
-	// }
-
-	export type { Row };
 </script>
 
 <script lang="ts">
 	export let cols: string[];
-	export let rows: Row[];
+	export let rows: unknown[];
 	export let striped: boolean = false;
 	export let hoverable: boolean = false;
 	export let scrollable: boolean = false;
