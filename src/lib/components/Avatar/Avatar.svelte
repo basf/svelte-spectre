b826b62eb0cee86dc6c8c018502d9afb48cedd10
Valexr <valexr@gmail.com>
2021-10-20 18:50:20 +0300

    back to the future

--- src/lib/components/Avatar/Avatar.svelte
+++ src/lib/components/Avatar/Avatar.svelte
@@ -1,4 +1,12 @@
-<figure class="avatar avatar-{size} text-{weight} {offset}" data-initial={initial}>
+<figure
+	class="avatar avatar-{size} text-{weight} {offset}"
+	data-initial={initials}
+	style="
+        background-color: {color};
+        font-size: {fontSize}px;
+        color: {color.isLight() ? '#000' : '#fff'}
+    "
+>
 	<slot />
 	{#if $$slots.sub}
 		<span class="avatar-icon">
@@ -6,29 +14,47 @@
 		</span>
 	{/if}
 	{#if status}<i class="avatar-presence {status}" />{/if}
+	{#if caption || $$slots.caption}
+		<figcaption class="text-dark">
+			<slot name="caption">{name}</slot>
+		</figcaption>
+	{/if}
 </figure>
 
 <script lang="ts" context="module">
+	import tinycolor from 'tinycolor2';
 	import type { Size } from '../../types/size';
 	import type { Weight } from '../../types/text';
 	import type { Offset } from '../../types/position';
-
-	type Status = 'online' | 'busy' | 'away' | 'offline' | false;
-
-	export type { Offset, Size, Status, Weight };
+	import { SIZE } from '../../types/const';
+	export type Status = 'online' | 'busy' | 'away' | 'offline' | false;
+	export type { Offset, Size, Weight };
 </script>
 
 <script lang="ts">
+	export let name: string = '';
+	export let bg: string = '#f6f6f6';
+	export let len: number = 0;
+	export let caption: boolean = false;
 	export let size: Size = 'md';
 	export let weight: Weight = 'normal';
 	export let status: Status = false;
-	export let initial: string = '';
 	export let offset: Offset = '';
+	let words: RegExpMatchArray;
+	let clip: number;
+	let fontSize: number;
+	let initials: string;
+	$: color = bg ? tinycolor(bg) : tinycolor.random();
+	$: words = name.replace('.', '/').match(/\b(\w)|([A-Z])|(\/)/g);
+	$: clip = len || words.length;
+	$: fontSize = SIZE[size] * (1 / clip);
+	$: initials = words.slice(0, clip).join('').toUpperCase();
 </script>
 
 <style lang="scss">
 	@import 'spectre.css/src/avatars';
-	// @import 'spectre.css/src/utilities';
+	@import 'spectre.css/src/utilities';
+
 	:global(.spectre) {
 		.avatar {
 			:global(img) {
@@ -42,6 +68,15 @@
 				border-radius: 50%;
 				display: flex;
 			}
+		}
+		figcaption {
+			position: absolute;
+			top: 100%;
+			left: 50%;
+			transform: translate(-50%, 0);
+			font-size: 80%;
+			color: black;
+			text-align: center;
 		}
 	}
 </style>
