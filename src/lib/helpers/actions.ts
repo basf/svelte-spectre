16e166754304068979d20ff4dcbda9536253394a
Valexr <valexr@gmail.com>
2021-10-20 14:40:23 +0300

    remove undeed action

--- src/lib/helpers/actions.ts
+++ src/lib/helpers/actions.ts
@@ -3,11 +3,4 @@ function addClass(node: HTMLElement, classes: string[]) {
     node.classList.add(...classes);
 }
 
-function addBadge(node: HTMLElement, badge: string) {
-    if (badge) {
-        node.classList.add('badge');
-        badge.length && node.setAttribute("data-badge", badge);
-    }
-}
-
-export { addClass, addBadge };
+export { addClass };
