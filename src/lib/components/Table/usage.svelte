09348484b0f6cc00b779bede4554ef9789cc0505
Valexr <valexr@gmail.com>
2021-10-26 12:33:47 +0300

    add darfts with @pm proposal ideas

---
+++ src/lib/components/Table/usage.svelte
@@ -0,0 +1,55 @@
+<Table cols={['foo', 'bar', 'baz']} rows={[1, 2, 3]} />
+
+<Table rows={[{ foo: 1, bar: 2, baz: 3 }]} />
+
+<Table cols={['foo', 'bar', 'baz']} rows={[{ foo: 1, bar: 2, baz: 3 }]} />
+
+<Table
+	cols={[
+		{ key: 'foo', label: 'Foo' },
+		{ key: 'bar', label: 'Bar' },
+		{ key: 'baz', label: 'Baz' },
+	]}
+	rows={[{ foo: 1, bar: 2, baz: 3 }]}
+/>
+
+<Table
+	cols={[
+		{ key: 'foo', label: 'Foo' },
+		{ key: 'bar', label: 'Bar' },
+		{ key: 'baz', label: 'Baz' },
+	]}
+	rows={[{ foo: 1, bar: 2, baz: 3 }]}
+	let:item
+>
+	<span>{item}</span>
+</Table>
+
+<Table
+	cols={[
+		{ key: ['foo', 'bar'], label: 'Foo + Bar' },
+		{ key: 'baz', label: 'Baz' },
+	]}
+	rows={[{ foo: 1, bar: 2, baz: 3 }]}
+	let:item
+	let:colIndex
+>
+	{#if colIndex === 0}
+		<span>{item.foo} + {item.bar}</span>
+	{:else}
+		<i>{item}</i>
+	{/if}
+</Table>
+
+<Table>
+	<div slot="head" let:col>
+		{col.label}
+		{#if $query.sort === col.key}
+			<IconButton icon={$query.order === 'ASC' ? 'arrow-up' : 'arrow-down'} />
+		{/if}
+	</div>
+</Table>
+
+<script lang="ts">
+	import Table from './draft.svelte';
+</script>
