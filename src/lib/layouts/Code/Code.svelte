be200fb4ce8612114a2da9e4af89c83b154b3a40
Valexr <valexr@gmail.com>
2022-01-28 12:50:58 +0300

    Code use:action

--- src/lib/layouts/Code/Code.svelte
+++ src/lib/layouts/Code/Code.svelte
@@ -1,11 +1,18 @@
-<pre {...$$restProps} class="code" data-lang={lang}>
-    <code>
-        <slot />
-    </code>
-</pre>
+{#if action}
+	<pre {...$$restProps} class="code" data-lang={lang} use:action={options}>
+		<code><slot {code} /></code>
+	</pre>
+{:else}
+	<pre {...$$restProps} class="code" data-lang={lang}>
+		<code><slot {code} /></code>
+	</pre>
+{/if}
 
 <script lang="ts">
 	export let lang: string = '';
+	export let code: string = '';
+	export let action: Function;
+	export let options: any;
 </script>
 
 <style lang="scss">
