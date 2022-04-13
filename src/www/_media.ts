055dd2dc5fa624aecb0c532139dcf32bddc91c07
Valexr <valexr@gmail.com>
2022-04-13 16:56:08 +0300

    add custom media store

--- src/www/_media.ts
+++ src/www/_media.ts
@@ -1,6 +1,9 @@
-import watchMedia from 'svelte-media';
+import { writable } from 'svelte/store';
 
-const mediaqueries = {
+type MediaQuery = {
+    [key: string]: boolean | string;
+};
+const mediaQueries: MediaQuery = {
     xs: '(max-width: 480px)',
     sm: '(max-width: 600px)',
     md: '(max-width: 840px)',
@@ -15,4 +18,38 @@ const mediaqueries = {
     touch: '(hover: none)',
 };
 
-export const media = watchMedia(mediaqueries);
+export const media = watchMedia(mediaQueries, 'ss-media')
+
+function watchMedia(mediaQueries: MediaQuery, storeKey: string) {
+    const { subscribe, set, update } = writable(mediaQueries);
+
+    const match: MediaQuery = storageTest() && JSON.parse(sessionStorage.getItem(storeKey) as string);
+
+    if (storageTest())
+        for (const query in mediaQueries) {
+            const media = window.matchMedia(mediaQueries[query] as string)
+            setMatches(media, query)
+            media.onchange = (e) => setMatches(e, query)
+        }
+
+    subscribe((match): void => storageTest() && sessionStorage.setItem(storeKey, JSON.stringify(match)));
+
+    function setMatches(source: MediaQueryList | MediaQueryListEvent, query: string) {
+        if ('target' in source) match[query] = source.matches;
+        else match[query] ??= source.matches;
+        set(match);
+        sessionStorage.setItem(storeKey, JSON.stringify(match));
+    }
+
+    function storageTest(test = 'test'): boolean {
+        try {
+            sessionStorage.setItem(test, test);
+            sessionStorage.removeItem(test);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    return { subscribe, set, update }
+}
