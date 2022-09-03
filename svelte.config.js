56cbb251eac595a8c0746ede0830a120ff2ffdff
Valexr <valexr@gmail.com>
2022-09-03 08:13:56 +0300

    fix dev

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import preprocess from 'svelte-preprocess';
 import mdsvexConfig from './mdsvex.config.js';
 import adapterStatic from '@sveltejs/adapter-static';
 
-// const mode = process.env.NODE_ENV;
-// const dev = mode === 'development';
 const dev = process.env.npm_lifecycle_event === 'dev';
 
 const primaryColor = process.argv.includes('package') ? '' : '$primary-color: hsl(215, 15%, 33%);';
