c9727e44fadfdfc85c7f405f01ec3311376d6490
Glisten**STAR** <glistenstar00@gmail.com>
2022-12-02 17:00:26 +0100

    add json for periodic table elements

--- src/lib/components/PeriodicTable/PeriodicTable.svelte
+++ src/lib/components/PeriodicTable/PeriodicTable.svelte
@@ -1,368 +1,29 @@
 <div {...$$restProps} class="columns">
 	<div id="periodictable">
 		<ul>
-			<!-- Select triangle button for coloumn -->
-			<li class="empty" />
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_1')}
-				on:mouseenter={() => selectRow('col_1')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_2')}
-				on:mouseenter={() => selectRow('col_2')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_3')}
-				on:mouseenter={() => selectRow('col_3')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_4')}
-				on:mouseenter={() => selectRow('col_4')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_5')}
-				on:mouseenter={() => selectRow('col_5')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_6')}
-				on:mouseenter={() => selectRow('col_6')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_7')}
-				on:mouseenter={() => selectRow('col_7')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_8')}
-				on:mouseenter={() => selectRow('col_8')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_9')}
-				on:mouseenter={() => selectRow('col_9')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_10')}
-				on:mouseenter={() => selectRow('col_10')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_11')}
-				on:mouseenter={() => selectRow('col_11')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_12')}
-				on:mouseenter={() => selectRow('col_12')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_13')}
-				on:mouseenter={() => selectRow('col_13')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_14')}
-				on:mouseenter={() => selectRow('col_14')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_15')}
-				on:mouseenter={() => selectRow('col_15')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_16')}
-				on:mouseenter={() => selectRow('col_16')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_17')}
-				on:mouseenter={() => selectRow('col_17')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<li
-				class="triangle_col"
-				on:mouseleave={() => deSelectRow('col_18')}
-				on:mouseenter={() => selectRow('col_18')}
-			>
-				<Icon icon={'arrow-down'} />
-			</li>
-			<!-- Period 1 -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_1')}
-				on:mouseenter={() => selectRow('row_1')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li data-pos="1" class="type_1 cat_2 row_1 col_1">H<span>Hydrogen</span></li>
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li data-pos="2" class="type_2 cat_2 row_1 col_18">He<span>Helium</span></li>
-			<!-- Period 2 -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_2')}
-				on:mouseenter={() => selectRow('row_2')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li data-pos="3" class="type_3 cat_0 row_2 col_1">Li<span>Lithium</span></li>
-			<li data-pos="4" class="type_4 cat_0 row_2 col_2">Be<span>Beryllium</span></li>
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li data-pos="5" class="type_5 cat_0 row_2 col_13">B<span>Boron</span></li>
-			<li data-pos="6" class="type_5 cat_0 row_2 col_14">C<span>Carbon</span></li>
-			<li data-pos="7" class="type_5 cat_2 row_2 col_15">N<span>Nitrogen</span></li>
-			<li data-pos="8" class="type_5 cat_2 row_2 col_16">O<span>Oxygen</span></li>
-			<li data-pos="9" class="type_5 cat_2 row_2 col_17">F<span>Fluorine</span></li>
-			<li data-pos="10" class="type_2 cat_2 row_2 col_18">Ne<span>Neon</span></li>
-			<!-- Period 3 -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_3')}
-				on:mouseenter={() => selectRow('row_3')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li data-pos="11" class="type_3 cat_0 row_3 col_1">Na<span>Sodium</span></li>
-			<li data-pos="12" class="type_4 cat_0 row_3 col_2">Mg<span>Magnesium</span></li>
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li data-pos="13" class="type_7 cat_0 row_3 col_13">Al<span>Aluminium</span></li>
-			<li data-pos="14" class="type_5 cat_0 row_3 col_14">Si<span>Silicon</span></li>
-			<li data-pos="15" class="type_5 cat_0 row_3 col_15">P<span>Phosphorus</span></li>
-			<li data-pos="16" class="type_5 cat_0 row_3 col_16">S<span>Sulfur</span></li>
-			<li data-pos="17" class="type_5 cat_2 row_3 col_17">Cl<span>Chlorine</span></li>
-			<li data-pos="18" class="type_2 cat_2 row_3 col_18">Ar<span>Argon</span></li>
-			<!-- Period 4 -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_4')}
-				on:mouseenter={() => selectRow('row_4')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li data-pos="19" class="type_3 cat_0 row_4 col_1">K<span>Potassium</span></li>
-			<li data-pos="20" class="type_4 cat_0 row_4 col_2">Ca<span>Calcium</span></li>
-			<li data-pos="21" class="type_6 cat_0 row_4 col_3">Sc<span>Scandium</span></li>
-			<li data-pos="22" class="type_6 cat_0 row_4 col_4">Ti<span>Titanium</span></li>
-			<li data-pos="23" class="type_6 cat_0 row_4 col_5">V<span>Vanadium</span></li>
-			<li data-pos="24" class="type_6 cat_0 row_4 col_6">Cr<span>Chromium</span></li>
-			<li data-pos="25" class="type_6 cat_0 row_4 col_7">Mn<span>Manganese</span></li>
-			<li data-pos="26" class="type_6 cat_0 row_4 col_8">Fe<span>Iron</span></li>
-			<li data-pos="27" class="type_6 cat_0 row_4 col_9">Co<span>Cobalt</span></li>
-			<li data-pos="28" class="type_6 cat_0 row_4 col_10">Ni<span>Nickel</span></li>
-			<li data-pos="29" class="type_6 cat_0 row_4 col_11">Cu<span>Copper</span></li>
-			<li data-pos="30" class="type_6 cat_0 row_4 col_12">Zn<span>Zinc</span></li>
-			<li data-pos="31" class="type_7 cat_0 row_4 col_13">Ga<span>Gallium</span></li>
-			<li data-pos="32" class="type_7 cat_0 row_4 col_14">Ge<span>Germanium</span></li>
-			<li data-pos="33" class="type_5 cat_0 row_4 col_15">As<span>Arsenic</span></li>
-			<li data-pos="34" class="type_5 cat_0 row_4 col_16">Se<span>Selenium</span></li>
-			<li data-pos="35" class="type_5 cat_1 row_4 col_17">Br<span>Bromine</span></li>
-			<li data-pos="36" class="type_2 cat_2 row_4 col_18">Kr<span>Krypton</span></li>
-			<!-- Period 5 -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_5')}
-				on:mouseenter={() => selectRow('row_5')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li data-pos="37" class="type_3 cat_0 row_5 col_1">Rb<span>Rubidium</span></li>
-			<li data-pos="38" class="type_4 cat_0 row_5 col_2">Sr<span>Strontium</span></li>
-			<li data-pos="39" class="type_6 cat_0 row_5 col_3">Y<span>Yttrium</span></li>
-			<li data-pos="40" class="type_6 cat_0 row_5 col_4">Zr<span>Zirconium</span></li>
-			<li data-pos="41" class="type_6 cat_0 row_5 col_5">Nb<span>Niobium</span></li>
-			<li data-pos="42" class="type_6 cat_0 row_5 col_6">Mo<span>Molybdenum</span></li>
-			<li data-pos="43" class="type_6 cat_0 row_5 col_7">Tc<span>Technetium</span></li>
-			<li data-pos="44" class="type_6 cat_0 row_5 col_8">Ru<span>Ruthenium</span></li>
-			<li data-pos="45" class="type_6 cat_0 row_5 col_9">Rh<span>Rhodium</span></li>
-			<li data-pos="46" class="type_6 cat_0 row_5 col_10">Pd<span>Palladium</span></li>
-			<li data-pos="47" class="type_6 cat_0 row_5 col_11">Ag<span>Silver</span></li>
-			<li data-pos="48" class="type_6 cat_0 row_5 col_12">Cd<span>Cadmium</span></li>
-			<li data-pos="49" class="type_7 cat_0 row_5 col_13">In<span>Indium</span></li>
-			<li data-pos="50" class="type_7 cat_0 row_5 col_14">Sn<span>Tin</span></li>
-			<li data-pos="51" class="type_7 cat_0 row_5 col_15">Sb<span>Antimony</span></li>
-			<li data-pos="52" class="type_5 cat_0 row_5 col_16">Te<span>Tellurium</span></li>
-			<li data-pos="53" class="type_5 cat_0 row_5 col_17">I<span>Iodine</span></li>
-			<li data-pos="54" class="type_2 cat_2 row_5 col_18">Xe<span>Xenon</span></li>
-			<!-- Period 6 -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_6')}
-				on:mouseenter={() => selectRow('row_6')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li data-pos="55" class="type_3 cat_0 row_6 col_1">Cs<span>Caesium</span></li>
-			<li data-pos="56" class="type_4 cat_0 row_6 col_2">Ba<span>Barium</span></li>
-			<li data-pos="71" class="type_6 cat_0 row_6 col_3">Lu<span>Lutetium</span></li>
-			<li data-pos="72" class="type_6 cat_0 row_6 col_4">Hf<span>Hafnium</span></li>
-			<li data-pos="73" class="type_6 cat_0 row_6 col_5">Ta<span>Tantalum</span></li>
-			<li data-pos="74" class="type_6 cat_0 row_6 col_6">W<span>Tungsten</span></li>
-			<li data-pos="75" class="type_6 cat_0 row_6 col_7">Re<span>Rhenium</span></li>
-			<li data-pos="76" class="type_6 cat_0 row_6 col_8">Os<span>Osmium</span></li>
-			<li data-pos="77" class="type_6 cat_0 row_6 col_9">Ir<span>Iridium</span></li>
-			<li data-pos="78" class="type_6 cat_0 row_6 col_10">Pt<span>Platinum</span></li>
-			<li data-pos="79" class="type_6 cat_0 row_6 col_11">Au<span>Gold</span></li>
-			<li data-pos="80" class="type_6 cat_1 row_6 col_12">Hg<span>Mercury</span></li>
-			<li data-pos="81" class="type_7 cat_0 row_6 col_13">Tl<span>Thallium</span></li>
-			<li data-pos="82" class="type_7 cat_0 row_6 col_14">Pb<span>Lead</span></li>
-			<li data-pos="83" class="type_7 cat_0 row_6 col_15">Bi<span>Bismuth</span></li>
-			<li data-pos="84" class="type_7 cat_0 row_6 col_16">Po<span>Polonium</span></li>
-			<li data-pos="85" class="type_5 cat_0 row_6 col_17">At<span>Astatine</span></li>
-			<li data-pos="86" class="type_2 cat_2 row_6 col_18">Rn<span>Radon</span></li>
-			<!-- Period 7 -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_7')}
-				on:mouseenter={() => selectRow('row_7')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li data-pos="87" class="type_3 cat_0 row_7 col_1">Fr<span>Francium</span></li>
-			<li data-pos="88" class="type_4 cat_0 row_7 col_2">Ra<span>Radium</span></li>
-			<li data-pos="103" class="type_6 cat_0 row_7 col_3">Lr<span>Lawrencium</span></li>
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<li class="empty" />
-			<!-- Lanthanides -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_8')}
-				on:mouseenter={() => selectRow('row_8')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li class="empty" />
-			<li class="empty" />
-			<li data-pos="57" class="type_1 cat_0 row_8">La<span>Lanthanum</span></li>
-			<li data-pos="58" class="type_1 cat_0 row_8">Ce<span>Cerium</span></li>
-			<li data-pos="59" class="type_1 cat_0 row_8">Pr<span>Praseodymium</span></li>
-			<li data-pos="60" class="type_1 cat_0 row_8">Nd<span>Neodynium</span></li>
-			<li data-pos="61" class="type_1 cat_0 row_8">Pm<span>Promethium</span></li>
-			<li data-pos="62" class="type_1 cat_0 row_8">Sm<span>Samarium</span></li>
-			<li data-pos="63" class="type_1 cat_0 row_8">Eu<span>Europium</span></li>
-			<li data-pos="64" class="type_1 cat_0 row_8">Gd<span>Gadolinium</span></li>
-			<li data-pos="65" class="type_1 cat_0 row_8">Tb<span>Terbium</span></li>
-			<li data-pos="66" class="type_1 cat_0 row_8">Dy<span>Dysprosium</span></li>
-			<li data-pos="67" class="type_1 cat_0 row_8">Ho<span>Holmium</span></li>
-			<li data-pos="68" class="type_1 cat_0 row_8">Er<span>Erbium</span></li>
-			<li data-pos="69" class="type_1 cat_0 row_8">Tm<span>Thulium</span></li>
-			<li data-pos="70" class="type_1 cat_0 row_8">Yb<span>Ytterbium</span></li>
-			<li class="empty" />
-			<li class="empty" />
-			<!-- Actinides -->
-			<li
-				class="triangle_row"
-				on:mouseleave={() => deSelectRow('row_9')}
-				on:mouseenter={() => selectRow('row_9')}
-			>
-				<Icon icon={'arrow-right'} />
-			</li>
-			<li class="empty" />
-			<li class="empty" />
-			<li data-pos="89" class="type_1 cat_0 row_9">Ac<span>Actinium</span></li>
-			<li data-pos="90" class="type_1 cat_0 row_9">Th<span>Thorium</span></li>
-			<li data-pos="91" class="type_1 cat_0 row_9">Pa<span>Protactinium</span></li>
-			<li data-pos="92" class="type_1 cat_0 row_9">U<span>Uranium</span></li>
-			<li data-pos="93" class="type_1 cat_0 row_9">Np<span>Neptunium</span></li>
-			<li data-pos="94" class="type_1 cat_0 row_9">Pu<span>Plutonium</span></li>
-			<li data-pos="95" class="type_1 cat_0 row_9">Am<span>Americium</span></li>
-			<li data-pos="96" class="type_1 cat_0 row_9">Cm<span>Curium</span></li>
-			<li data-pos="97" class="type_1 cat_0 row_9">Bk<span>Berkelium</span></li>
-			<li data-pos="98" class="type_1 cat_0 row_9">Cf<span>Californium</span></li>
-			<li data-pos="99" class="type_1 cat_0 row_9">Es<span>Einsteinium</span></li>
-			<li data-pos="100" class="type_1 cat_0 row_9">Fm<span>Fermium</span></li>
-			<li data-pos="101" class="type_1 cat_0 row_9">Md<span>Mendelevium</span></li>
-			<li data-pos="102" class="type_1 cat_0 row_9">No<span>Nobelium</span></li>
-			<li class="empty" />
-			<li class="empty" />
+			{#each table_data as item}
+				{#if item.type == 'element'}
+					<li data-pos={item['data-pos']} class={item['class']}>
+						{item.name}<span>{item.title}</span>
+					</li>
+				{/if}
+				{#if item.type == 'icon'}
+					<li
+						class={item['class']}
+						on:mouseleave={() => deSelectRow(item?.select)}
+						on:mouseenter={() => selectRow(item?.select)}
+					>
+						{#if item['class'] == 'triangle_col'}
+							<Icon icon={'arrow-down'} />
+						{:else if item['class'] == 'triangle_row'}
+							<Icon icon={'arrow-right'} />
+						{/if}
+					</li>
+				{/if}
+				{#if item.type == 'empty'}
+					<li class={item['class']} />
+				{/if}
+			{/each}
 		</ul>
 	</div>
 </div>
@@ -372,11 +33,12 @@
 
 <script lang="ts">
 	import { Icon } from '$lib';
+	import table_data from './table_data.json';
 
 	const selectRow = (id: string) => {
 		let divs = document.querySelectorAll(`#periodictable > ul > li.${id}`);
 		divs.forEach((item) => {
-			item.style.transform = 'scale(1.1)';
+			item.style.transform = 'scale(1.2)';
 			item.style.zIndex = '999999999999999999999';
 		});
 	};
@@ -384,8 +46,8 @@
 	const deSelectRow = (id: string) => {
 		let divs = document.querySelectorAll(`#periodictable > ul > li.${id}`);
 		divs.forEach((item) => {
-			item.style.transform = 'scale(1)';
-			item.style.zIndex = '1';
+			item.style.transform = '';
+			item.style.zIndex = 'revert-layer';
 		});
 	};
 </script>
