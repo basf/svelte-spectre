0600cf48f5df0f06d3e9f155090beddb2828c3bc
Evgeny Blokhin <eb@tilde.pro>
2026-07-05 14:43:56 +0200

    Change the codebase legal maintainer

--- src/www/+layout.svelte
+++ src/www/+layout.svelte
@@ -141,7 +141,8 @@
 		</svelte:fragment>
 
 		<footer class="text-center p-2">
-			&copy; 2021 &mdash; 2026 BASF SE, Materials Platform for Data Science O&Uuml;, and Absolidix FZ-LLC
+			&copy; 2021 &mdash; 2026 BASF SE, Materials Platform for Data Science O&Uuml;, and
+			Absolidix FZ-LLC
 		</footer>
 	</Sidebar>
 
